0116ec71e5383ddb3aafed3bae5fc1ce187bb150
Agdein <agn8781@gmail.com>
2025-01-16 13:57:11 +0300

    Sprite controller changes
    Добавлено затухание спрайта при переключении на другой
    Добавлено больше положений спрайта

--- Assets/Project_N/Skripts/Sprites/SpriteController.cs
+++ Assets/Project_N/Skripts/Sprites/SpriteController.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
+using Color = System.Drawing.Color;
 
 public class SpriteController : MonoBehaviour
 {
-     [SerializeField] private Camera _camera; // Ссылка на камеру, назначается в инспекторе
+   /* [SerializeField] private Camera _camera; // Ссылка на камеру, назначается в инспекторе
     [SerializeField] private Sprite directSprite; // Оригинальный спрайт
     [SerializeField] private Sprite turningRightSprite;
     [SerializeField] private Sprite turningLeftSprite; // Альтернативный спрайт
@@ -39,9 +41,9 @@ public class SpriteController : MonoBehaviour
     {
 
         float yRotation = transform.rotation.eulerAngles.y;
-        
+
             RotateTowardsCamera();
-    
+
             if ((yRotation >= 0 && yRotation <= 10) || (yRotation >= 70 && yRotation <= 90)|| (yRotation >= 170 && yRotation <= 190) || (yRotation >= 260 && yRotation <= 280) || (yRotation >= 350 && yRotation <= 360))
             {
                 SetDirectSprite();
@@ -54,7 +56,7 @@ public class SpriteController : MonoBehaviour
             }
             else if ((yRotation >= 45 && yRotation <= 90) || (yRotation >= 135 && yRotation <= 180) || (yRotation >= 225 && yRotation <= 270) || (yRotation >= 315 && yRotation <= 360))
             {
-                
+
                 SetTurningRightSprite();
                 ResetSprite();
             }
@@ -87,7 +89,7 @@ public class SpriteController : MonoBehaviour
 
     private void SetTurningRightSprite()
     {
-       
+
        if (spriteRenderer.sprite != turningRightSprite)
         {
             spriteRenderer.sprite = turningRightSprite;
@@ -118,6 +120,186 @@ public class SpriteController : MonoBehaviour
                     transform.localScale = CurrentPosition;
         }
 
-       
-    }
+
+    }  */
+
+   [SerializeField] private Sprite _frontFaceSprite;
+   [SerializeField] private Sprite _midFrontFaceSprite;
+   [SerializeField] private Sprite _middleFaceSprite;
+   [SerializeField] private Sprite _midSideFaceSprite;
+   [SerializeField] private Sprite _sideFaceSprite;
+
+   [SerializeField] private GameObject _angleMeterX;
+   [SerializeField] private GameObject _angleMeterY;
+   [SerializeField] private SpriteRenderer _subSpriteRenderer;
+
+   [SerializeField] private float ColorChangeSpeed = 0.2f;
+
+   private Camera _mainCamera;
+   private SpriteRenderer _spriteRenderer;
+   private Animator _animator;
+   private UnityEngine.Color _baseColor;
+
+   void Start()
+   {
+       if (_spriteRenderer == null)
+       {
+           _spriteRenderer = GetComponent<SpriteRenderer>();
+       }
+
+       if (_mainCamera == null)
+       {
+           _mainCamera = Camera.main;
+       }
+
+       _baseColor = _spriteRenderer.color;
+   }
+
+   void Update()
+   {
+      /* var a = _spriteRenderer.color;
+       a.a -= 1 ;
+       _spriteRenderer.color = UnityEngine.Color.Lerp(_spriteRenderer.color, a, Mathf.Abs(Mathf.Sin(Time.time)));
+*/
+
+      /* Debug.Log(_spriteRenderer.color.a);
+      if (!ololo)
+      {
+          var a = _spriteRenderer.color;
+          a.a -= ColorChangeSpeed;
+          _spriteRenderer.color = a;
+
+          if (_spriteRenderer.color.a <= 0)
+          {
+              ololo = true;
+          }
+
+
+      }
+      if (ololo)
+      {
+          var a = _spriteRenderer.color;
+          a.a += ColorChangeSpeed;
+          _spriteRenderer.color = a;
+
+          if (_spriteRenderer.color.a >= 1)
+          {
+              ololo = false;
+          }
+      }
+      */
+
+      float yRotation = _angleMeterY.transform.rotation.eulerAngles.y;
+
+
+     RotateTowardsCamera();
+
+      if ((yRotation >= 345 && yRotation <= 360) || (yRotation >= 0 && yRotation <= 15) ||
+         (yRotation >= 165 && yRotation <= 195))
+      {
+         ChangeSprite(_frontFaceSprite, yRotation);
+      }
+      else if((yRotation >= 75 && yRotation <= 105)|| (yRotation >= 255 && yRotation <= 285))
+      {
+          if (_sideFaceSprite != null)
+          {
+              ChangeSprite(_sideFaceSprite, yRotation);
+          }
+          else
+          {
+              ChangeSprite(_frontFaceSprite, yRotation);
+          }
+      }
+      else if((yRotation >= 30 && yRotation <= 60) || (yRotation >= 120 && yRotation <= 150) ||
+              (yRotation >= 210 && yRotation <= 240) || (yRotation >= 300 && yRotation <= 330))
+      {
+         ChangeSprite(_middleFaceSprite, yRotation);
+      }
+      else if ((yRotation > 15 && yRotation < 30) || (yRotation > 150 && yRotation < 165) ||
+               (yRotation > 195 && yRotation < 210) || (yRotation > 330 && yRotation < 345))
+      {
+          ChangeSprite(_midFrontFaceSprite, yRotation);
+      }
+      else
+      {
+          if (_midSideFaceSprite != null)
+          {
+              ChangeSprite(_midSideFaceSprite, yRotation);
+          }
+          else
+          {
+              ChangeSprite(_midFrontFaceSprite, yRotation);
+          }
+
+      }
+   }
+
+   private void RotateTowardsCamera() //Поворот спрайта к камере
+   {
+       var camPosition = _mainCamera.transform.position;
+       camPosition.y = transform.position.y;
+      _angleMeterY.transform.LookAt(camPosition);
+   }
+
+   private void ChangeSprite(Sprite sprite, float angle)
+   {
+       bool flip = CheckFlipSprite(angle);
+
+       if (_spriteRenderer.sprite != sprite)
+       {
+
+           _subSpriteRenderer.enabled = true;
+           _subSpriteRenderer.sprite = sprite;
+           if (flip)
+           {
+               _subSpriteRenderer.flipX = true;
+           }
+           else
+           {
+               _subSpriteRenderer.flipX = false;
+           }
+
+           if (_spriteRenderer.color.a <= 0)
+           {
+               if (flip)
+               {
+                   _spriteRenderer.flipX = true;
+               }
+               else
+               {
+                   _spriteRenderer.flipX = false;
+               }
+               _spriteRenderer.color = _baseColor;
+               _spriteRenderer.sprite = sprite;
+           }
+           else
+           {
+               FadingMainSprite();
+           }
+
+       }
+       else
+       {
+        //Debug.Log(sprite.name);
+       }
+
+   }
+   private bool CheckFlipSprite(float angle)
+   {
+
+       if ((angle > 105 && angle < 195) || (angle > 255 && angle < 345))
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+   }
+   private void FadingMainSprite()
+               {
+                   var alfaColor = _spriteRenderer.color;
+                   alfaColor.a -= ColorChangeSpeed;
+                   _spriteRenderer.color = alfaColor;
+               }
 }
